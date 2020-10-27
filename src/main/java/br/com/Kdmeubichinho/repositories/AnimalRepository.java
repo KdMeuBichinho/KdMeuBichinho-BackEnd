62252bdd3e155546e328b0485cfef951c5d815f4
Luciana Lemos <luarlemos96@hotmail.com>
2020-10-27 19:20:24 -0300

    Desenvolvimento api pessoa (get, getbyid, put, post, getbyemail, delete)

--- src/main/java/br/com/Kdmeubichinho/repositories/AnimalRepository.java
+++ src/main/java/br/com/Kdmeubichinho/repositories/AnimalRepository.java
@@ -2,7 +2,7 @@ package br.com.Kdmeubichinho.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-import br.com.Kdmeubichinho.models.entities.Animal;
+import br.com.Kdmeubichinho.entities.Animal;
 
 public interface AnimalRepository extends CrudRepository<Animal, Integer>{
 
